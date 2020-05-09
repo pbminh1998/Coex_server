6483886b71e47812b7e183747e485496b8efb744
pbminh1998 <pbminh1998@gmail.com>
2020-05-09 16:38:54 +0700

    make it better

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,9 @@ export async function main(options: ApplicationConfig = {
   await app.boot();
   await app.start();
 
-  Schedule.agenda = new Agenda({ db: { address: `mongodb://${database.user + database.user!=''?':':''}${database.password + database.user!=''?'@':''}${database.host}:${database.port}/${database.database}` } });
+  console.log(database.user);
+
+  Schedule.agenda = new Agenda({ db: { address: `mongodb://${database.user + (database.user != ''?':':'')}${database.password + (database.user!=''?'@':'')}${database.host}:${database.port}/${database.database}` } });
   Schedule.agenda.on("ready", function () {
     let schedule = new Schedule(app.getSync<TransactionRepository>('repositories.TransactionRepository'), app.getSync<BookingRepository>('repositories.BookingRepository'), app.getSync<UserRepository>('repositories.UserRepository'));
     schedule.define();
